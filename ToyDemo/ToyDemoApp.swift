7405bdb10c452e75c722c74b7b7b2d4899347c1f
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2022-02-27 19:04:58 +0200

    Initial Commit

---
+++ ToyDemo/ToyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyDemoApp.swift
+//  ToyDemo
+//
+//  Created by Hager Elsayed on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
